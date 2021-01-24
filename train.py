7c9f7c57d9fc9f46822f140bb98ad26e6dbf9fb1
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-24 19:05:07 +0530

    Imported dataset

---
+++ train.py
@@ -0,0 +1,14 @@
+# MOBILE PRICE RANGE CLASSIFICATION
+
+# DEVELOPED BY
+# MOULISHANKAR M R 
+
+#IMPORTING MODULES
+mport numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# IMPORTING MODULES
+data = pd.read_csv("data/data.csv")
