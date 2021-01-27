2b0cb1f04c524e487cdaf6138bdef73e5e1c662c
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-27 21:07:29 +0530

    Designed Neural Network

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import save_model 
 import matplotlib.pyplot as plt
 
-# IMPORTING MODULES
+# IMPORTING DATA
 data = pd.read_csv("mobile_data.csv")
 
 # PREPROCESSING DATA
@@ -24,5 +24,17 @@ y = data.iloc[:,12].values
 # SPLITTING THE TRAINING AND VALIDATION DATA
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.1, random_state = 0)
 
+# BUILDING THE NEURAL NETWORK
+model = Sequential()
+model.add(Dense(64, input_dim = 12, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(4, activation='softmax'))
+
+# TRAINING THE DATA
+opt = Adam(learning_rate = 0.01)
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+history = model.fit(x_train, y_train, epochs = 150, batch_size = 32, validation_data = (x_val, y_val))
+print("\n\n ----- Model is trained successfully ! ----- \n\n")
 
 
